ee2de88508088975bb7824707a3d5ea2a023bcab
윤용식 <comnori@gmail.com>
2024-01-08 12:12:44 +0900

    remove unused icom components

--- src/components/atoms/icons/BxMoonOutlined.jsx
+++
@@ -1,9 +0,0 @@
-import Icon from "@ant-design/icons/lib/components/Icon"
-import { IconBxMoonSVG } from "assets/icons/CustomIcons"
-import * as React from "react"
-
-function BxMoonOutlined() {
-  return <Icon component={IconBxMoonSVG} />
-}
-
-export default BxMoonOutlined
