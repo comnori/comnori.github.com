a0f20d746ebf542ac8b47e840b29a961926243f3
윤용식 <comnori@gmail.com>
2024-01-16 22:57:32 +0900

    import용 markdown 문서는 sitemap 에 적용되지 않도록 변경

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -21,7 +21,7 @@ export const createPages = async ({ graphql, actions, reporter }) => {
 
   const result = await graphql(`
     query {
-      allMdx {
+      allMdx(filter: {frontmatter: {slug: {ne: null}}}) {
         nodes {
           id
           frontmatter {
@@ -44,16 +44,10 @@ export const createPages = async ({ graphql, actions, reporter }) => {
   const postsBase = "/blog"
   const postTemplate = path.resolve("./src/components/templates/PostTemplate.jsx")
 
-  // you'll call `createPage` for each result
   posts.forEach(node => {
     createPage({
-      // As mentioned above you could also query something else like frontmatter.title above and use a helper function
-      // like slugify to create a slug
       path: `${postsBase}${node.frontmatter.slug}`,
-      // Provide the path to the MDX content file so webpack can pick it up and transform it into JSX
       component: `${postTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
-      // You can use the values in this context in
-      // our page layout component
       context: { id: node.id },
     })
   })
